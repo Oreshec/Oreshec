d4160e0d3fd6b8841d6df8c161539ad8beda3c21
Oreshec <vlad.mumiay12@mail.ru>
2024-03-30 04:44:30 +0300

    Добавил отлов ошибок и теперь она не закрывается

--- group_chat_get_user_info.py
+++ group_chat_get_user_info.py
@@ -1,4 +1,5 @@
 import vk_api
+import traceback
 
 from key import main_token
 
@@ -7,33 +8,38 @@ vk = session.get_api()
 
 
 def group_chat_get_user_info():
-    chat_id = input('Введите ID чата ')
+    try:
+        while True:
+            chat_id = input('Введите ID чата: ')
+            if chat_id == "Exit":
+                break
+            else:
+                chat = vk.messages.getChat(chat_id=chat_id)
+                chat_title = chat["title"]
+                chat_id = chat["id"]
+                chat_users = chat["users"]
+                print('Title: ', chat_title)
+                print('ID: ', chat_id)
 
-    chat = vk.messages.getChat(chat_id=chat_id)
-    chat_title = chat["title"]
-    chat_id = chat["id"]
-    chat_users = chat["users"]
-
-    print('Title: ', chat_title)
-    print('ID: ', chat_id)
-
-    n = len(chat_users)
-    l = 0
-    b = 0
-    while l < n:
-        if chat_users[l] > 0:
-            user = vk.users.get(user_ids=chat_users)
-            print(l + 1)
-            print(user[l]['first_name'] + ' ' + user[l]['last_name'])
-            print(user[l]['id'])
-            l = l + 1
-        elif chat_users[l] < 0:
-            n = n - 1
-            l = l + 1
-            b = b + 1
-    print(b)
-    print('Done')
-
+                n = len(chat_users)
+                l = 0
+                b = 0
+                while l < n:
+                    if chat_users[l] > 0:
+                        user = vk.users.get(user_ids=chat_users)
+                        print(l + 1)
+                        print(user[l]['first_name'] + ' ' + user[l]['last_name'])
+                        print(user[l]['id'])
+                        l = l + 1
+                    elif chat_users[l] < 0:
+                        n = n - 1
+                        l = l + 1
+                        b = b + 1
+                print(b)
+                print('Done')
+    except vk_api.exceptions.ApiError:
+        print(f'Диалога {chat_id} не существует!')
+        group_chat_get_user_info()
 
 if __name__ == '__main__':
     group_chat_get_user_info()
