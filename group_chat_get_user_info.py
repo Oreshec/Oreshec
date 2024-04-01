90498eb80803bd90d0b07a1b65d806bf54acd87d
Oreshec <vlad.mumiay12@mail.ru>
2024-04-02 02:56:27 +0300

    Смена while на for

--- group_chat_get_user_info.py
+++ group_chat_get_user_info.py
@@ -1,43 +1,40 @@
 import vk_api
-import traceback
 
 from config import main_token
 
-session = vk_api.VkApi(token=main_token)
-vk = session.get_api()
-
+def enumeration(user, chat_users):
+    try:
+        for i in range(len(chat_users)):
+            print(f'№{i+1}')
+            print('\tName: ', user[i]['first_name'] + ' ' + user[i]['last_name'])
+            print('\tID: ' ,user[i]['id'])
+    except IndexError as e:
+        print('Error',e)
+        pass
 
 def group_chat_get_user_info():
+    session = vk_api.VkApi(token=main_token)
+    vk = session.get_api()
     try:
         while True:
             chat_id = input('Введите ID чата: ')
-            if chat_id == "Exit":
+            if chat_id.lower() == "exit":
                 break
             else:
                 chat = vk.messages.getChat(chat_id=chat_id)
                 chat_title = chat["title"]
                 chat_id = chat["id"]
                 chat_users = chat["users"]
+                
+                user = vk.users.get(user_ids=chat_users)
+                
                 print('Title: ', chat_title)
                 print('ID: ', chat_id)
-
-                n = len(chat_users)
-                l = 0
-                b = 0
-                while l < n:
-                    if chat_users[l] > 0:
-                        user = vk.users.get(user_ids=chat_users)
-                        print(l + 1)
-                        print(user[l]['first_name'] + ' ' + user[l]['last_name'])
-                        print(user[l]['id'])
-                        l = l + 1
-                    elif chat_users[l] < 0:
-                        n = n - 1
-                        l = l + 1
-                        b = b + 1
-                print(b)
-                print('Done')
-    except vk_api.exceptions.ApiError:
+                print('Количество участников: ', len(chat_users))
+                enumeration(user, chat_users)
+                            
+    except vk_api.exceptions.ApiError as e:
+        print(e)
         print(f'Диалога {chat_id} не существует!')
         group_chat_get_user_info()
 
