bd7e419fc5b6d9e20aa0d8971994b2b4a7b891f8
Oreshec <vlad.mumiay12@mail.ru>
2024-04-02 01:33:15 +0300

    Не много переработана логика. Вроде как этот способ должен быть более безопасным

--- VK_Reader_Message.py
+++ VK_Reader_Message.py
@@ -6,14 +6,8 @@ import traceback
 # Token file
 from config import main_token
 
-# authorization
-vk_session = vk_api.VkApi(token=main_token)
-longpoll = VkLongPoll(vk_session)
-vk = vk_session.get_api()
-
-
 # definition is it a person or a bot by ID
-def user_detector(event):
+def user_detector(event, vk):
     try:
         if event.user_id > 0:
             user = vk.users.get(user_ids=event.user_id)  # Получаем информацию о пользователе
@@ -32,7 +26,7 @@ def user_detector(event):
 
 
 # Получаем информацию о Беседе
-def chat_conversation(chat_id):
+def chat_conversation(chat_id, vk):
     chat = vk.messages.getChat(chat_id=chat_id)  # Даем запрос об информации о беседе
     chat_title = chat["title"]  # Название беседы
     chat_id = str(chat["id"])  # ID беседы
@@ -40,7 +34,7 @@ def chat_conversation(chat_id):
     print(chat_info)
 
 
-def group_detector(event):
+def group_detector(event,vk):
     try:
         group_id = event.group_id
         group = vk.groups.getAddresses(group_id=group_id)
@@ -51,62 +45,69 @@ def group_detector(event):
 
 
 def main():
-    print("Worked")
-    print()
+    vk_session = vk_api.VkApi(token=main_token)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+
+    print(f"Worked")
 
     for event in longpoll.listen():  # Запускаем процесс "прослушки"
 
         if event.type == VkEventType.MESSAGE_NEW:  # Проверяем на условие нового сообщения
-
+        
             print('New Message!')
             print(time.asctime())
 
             if event.from_me:
                 print('From me for:', end=' ')
-                user_detector(event)
+                user_detector(event,vk)
             elif event.to_me:
                 print('For me from:', end=' ')
-                user_detector(event)
+                user_detector(event,vk)
             elif event.from_user:
                 print('From the user:', end=' ')
-                user_detector(event)
+                user_detector(event,vk)
 
             if event.from_chat:
                 print('in chat:', end=' ')
-                chat_conversation(event.chat_id)
+                chat_conversation(event.chat_id,vk)
             elif event.from_group:
                 print('groups:', end=' ')
-                group_detector(event)
-            print('Text:', event.text, ' ')
-            print()
+                group_detector(event,vk)
+                
+            if event.text != "":
+                print('Text:', event.text, ' ')
+                print()
+            else:
+                print(f'Нет текста!\n')
 
-            # the user types a message
-        if event.type == VkEventType.USER_TYPING:
-            print(time.asctime(), end=' ')
-            user_detector(event)
-            print('Typing... ')
-            if event.from_user:
-                print(event.user_id)
-                user_detector(event)
-            elif event.from_group:
-                print('Group Administrator', event.group_id)
-        if event.type == VkEventType.USER_TYPING_IN_CHAT:
-            print(time.asctime())
-            print('Typing ')
-            user_detector(event)
-            print('in chat', end=' ')
-            chat_conversation(event.chat_id)
+                # the user types a message
+            if event.type == VkEventType.USER_TYPING:
+                print(time.asctime(), end=' ')
+                user_detector(event,vk)
+                print('Typing... ')
+                if event.from_user:
+                    print(event.user_id)
+                    user_detector(event,vk)
+                elif event.from_group:
+                    print('Group Administrator', event.group_id)
+            if event.type == VkEventType.USER_TYPING_IN_CHAT:
+                print(time.asctime())
+                print('Typing ')
+                user_detector(event,vk)
+                print('in chat', end=' ')
+                chat_conversation(event.chat_id,vk)
 
-        if event.type == VkEventType.USER_ONLINE:
-            print(time.asctime())
-            print('User')
-            user_detector(event)
-            print('online', event.platform)
-        if event.type == VkEventType.USER_OFFLINE:
-            print(time.asctime())
-            print('User')
-            user_detector(event)
-            print('offline', event.offline_type)
+            if event.type == VkEventType.USER_ONLINE:
+                print(time.asctime())
+                print('User')
+                user_detector(event,vk)
+                print('online', event.platform)
+            if event.type == VkEventType.USER_OFFLINE:
+                print(time.asctime())
+                print('User')
+                user_detector(event,vk)
+                print('offline', event.offline_type)
 
 
 if __name__ == '__main__':
