9413921a63244ab1f50ee8487888e0514a33975b
Oreshec <vlad.mumiay12@mail.ru>
2024-04-02 08:11:22 +0300

    Отступ

--- group_chat_get_user_info.py
+++ group_chat_get_user_info.py
@@ -2,20 +2,22 @@ import vk_api
 
 from config import main_token
 
+
 def enumeration(user, chat_users):
     try:
         for i in range(len(chat_users)):
-            print(f'№{i+1}')
+            print(f'№{i + 1}')
             print('\tName: ', user[i]['first_name'] + ' ' + user[i]['last_name'])
-            print('\tID: ' ,user[i]['id'])
+            print('\tID: ', user[i]['id'])
     except IndexError as e:
-        print('Error',e)
+        print('Error', e)
         pass
 
+
 def group_chat_get_user_info():
-    session = vk_api.VkApi(token=main_token)
-    vk = session.get_api()
     try:
+        session = vk_api.VkApi(token=main_token)
+        vk = session.get_api()
         while True:
             chat_id = input('Введите ID чата: ')
             if chat_id.lower() == "exit":
@@ -25,18 +27,19 @@ def group_chat_get_user_info():
                 chat_title = chat["title"]
                 chat_id = chat["id"]
                 chat_users = chat["users"]
-                
+
                 user = vk.users.get(user_ids=chat_users)
-                
+
                 print('Title: ', chat_title)
                 print('ID: ', chat_id)
                 print('Количество участников: ', len(chat_users))
                 enumeration(user, chat_users)
-                            
+
     except vk_api.exceptions.ApiError as e:
         print(e)
         print(f'Диалога {chat_id} не существует!')
         group_chat_get_user_info()
+
 
 if __name__ == '__main__':
     group_chat_get_user_info()
