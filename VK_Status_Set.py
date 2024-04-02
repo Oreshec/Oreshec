1f942ce31e64279b64a477e5b5204f686963a7bf
Oreshec <vlad.mumiay12@mail.ru>
2024-04-02 03:23:52 +0300

    Мелкие исправления

--- VK_Status_Set.py
+++ VK_Status_Set.py
@@ -2,17 +2,16 @@ import time
 import vk_api
 from config import main_token
 
-session = vk_api.VkApi(token=main_token)
-vk = session.get_api()
-
-
 def status_set():
-    profile_info = vk.account.getProfileInfo()
-    vk.status.set(text=time.asctime())
-    status = session.method("status.get", {"user_id": profile_info['id']})
-    print(status['text'])
-    time.sleep(300)
+    while True:
+        session = vk_api.VkApi(token=main_token)
+        vk = session.get_api()
+        profile_info = vk.account.getProfileInfo()
+        vk.status.set(text=time.asctime())
+        status = session.method("status.get", {"user_id": profile_info['id']})
+        print(status['text'])
+        time.sleep(300)
 
 
-while True:
+if __name__ == '__main__':
     status_set()
